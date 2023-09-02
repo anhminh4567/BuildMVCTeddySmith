3dbb83212d0cac565dc0e56b3072a367a1c2dc78
anhminh <minhtran052003@gmail.com>
2023-09-02 21:55:29 +0700

    day 1 commit testt

--- Dto/ClassBayBaDungTestSonarCloud.cs
+++ Dto/ClassBayBaDungTestSonarCloud.cs
@@ -3,7 +3,6 @@
     public class ClassBayBaDungTestSonarCloud
     {
         public int whateverInt { get; set; }
-        public string whateverString { get; set; }
-        public DateTime DateTime { get; set; }
+
     }
 }
