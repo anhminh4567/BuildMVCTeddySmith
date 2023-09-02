4c7ca320aab08559995e9dc7ab860d6a934835c2
anhminh <minhtran052003@gmail.com>
2023-09-02 21:43:18 +0700

    them dai 1 class, test sonarcloud

---
+++ Dto/ClassBayBaDungTestSonarCloud.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildMVCTeddySmith.Dto
+{
+    public class ClassBayBaDungTestSonarCloud
+    {
+        public int whateverInt { get; set; }
+        public string whateverString { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
